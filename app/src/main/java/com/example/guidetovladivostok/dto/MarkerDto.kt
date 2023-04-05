a801393db04b43b490c4f28b4e3187482282ce87
kirlozavr <kirill.protchenko@mail.ru>
2023-04-05 23:20:06 +1000

    Дто маркера

---
+++ app/src/main/java/com/example/guidetovladivostok/dto/MarkerDto.kt
@@ -0,0 +1,40 @@
+package com.example.guidetovladivostok.dto
+
+import com.yandex.mapkit.geometry.Point
+
+class MarkerDto constructor(
+    documentId: String,
+    point: Point,
+    nameLocation: String,
+    informationLocation: String,
+    address: String
+) {
+
+    private var documentId: String
+    private var nameLocation: String
+    private var informationLocation: String
+    private var address: String
+    private var point: Point
+
+    init {
+        this.documentId = documentId
+        this.point = point
+        this.nameLocation = nameLocation
+        this.informationLocation = informationLocation
+        this.address = address
+    }
+
+    fun getDocumentId(): String = documentId
+
+    fun getPoint(): Point = point
+
+    fun getLatitude(): Double = point.latitude
+
+    fun getLongitude(): Double = point.longitude
+
+    fun getNameLocation(): String = nameLocation
+
+    fun getInformationLocation(): String = informationLocation
+
+    fun getAddress(): String = address
+}
