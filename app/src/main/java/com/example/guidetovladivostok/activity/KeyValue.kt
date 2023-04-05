6905112fc342a2228744f0ee387ee8639999b812
kirlozavr <kirill.protchenko@mail.ru>
2023-04-05 23:16:55 +1000

    Класс для доступа к ключам

---
+++ app/src/main/java/com/example/guidetovladivostok/activity/KeyValue.kt
@@ -0,0 +1,9 @@
+package com.example.guidetovladivostok.activity
+
+object KeyValue {
+    const val KEY_ADDRESS = "address"
+    const val KEY_NAME_LOCATION = "nameLocation"
+    const val KEY_LATITUDE = "latitude"
+    const val KEY_LONGITUDE = "longitude"
+    const val KEY_IS_CREATE = "isCreate"
+}
