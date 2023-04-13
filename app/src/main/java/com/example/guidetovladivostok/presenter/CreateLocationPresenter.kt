8cf13f7a9299471609f9b940cec0226faeaaadc4
kirlozavr <kirill.protchenko@mail.ru>
2023-04-13 22:04:30 +1000

    Презентер для работы с созданием локации в БД

---
+++ app/src/main/java/com/example/guidetovladivostok/presenter/CreateLocationPresenter.kt
@@ -0,0 +1,27 @@
+package com.example.guidetovladivostok.presenter
+
+import com.example.guidetovladivostok.entity.MarkerEntity
+import com.example.guidetovladivostok.model.CallBackOnClose
+import com.example.guidetovladivostok.model.CreateLocationModel
+
+class CreateLocationPresenter
+constructor(contractView: CreateLocationContract.View)
+    : CreateLocationContract.Presenter<MarkerEntity> {
+
+    private val contractView: CreateLocationContract.View
+    private val contractModel: CreateLocationContract.Model<MarkerEntity, CallBackOnClose>
+    private val callBackOnClose = object : CallBackOnClose{
+        override fun onClose() {
+            contractView.closeFragment()
+        }
+    }
+
+    init {
+        this.contractView = contractView
+        this.contractModel = CreateLocationModel()
+    }
+
+    override fun buttonCreateLocation(value: MarkerEntity) {
+        contractModel.putMarker(value, callBackOnClose)
+    }
+}
