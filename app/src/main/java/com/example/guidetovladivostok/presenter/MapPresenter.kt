8caf40c78604f45ea7f50e04ffaf46721a56bf42
kirlozavr <kirill.protchenko@mail.ru>
2023-04-13 22:02:15 +1000

    Добавил логику презентеру

--- app/src/main/java/com/example/guidetovladivostok/presenter/MapPresenter.kt
+++ app/src/main/java/com/example/guidetovladivostok/presenter/MapPresenter.kt
@@ -1,6 +1,25 @@
 package com.example.guidetovladivostok.presenter
 
-class MapPresenter {
+import com.example.guidetovladivostok.dto.MarkerDto
+import com.example.guidetovladivostok.model.CallBackHandler
+import com.example.guidetovladivostok.model.ListLocationModel
 
+class MapPresenter: MapContract.Presenter {
 
+    private lateinit var contractView: MapContract.View<List<MarkerDto>>
+    private val contractModel: MapContract.Model<CallBackHandler<List<MarkerDto>>>
+    private val callBackHandler = object : CallBackHandler<List<MarkerDto>>{
+        override fun execute(value: List<MarkerDto>) {
+            contractView.showAllMarkers(value)
+        }
+    }
+
+    constructor(contractView: MapContract.View<List<MarkerDto>>){
+        this.contractView = contractView
+        this.contractModel = ListLocationModel()
+    }
+
+    override fun showAllMarkers() {
+        contractModel.getListLocation(callBackHandler)
+    }
 }
