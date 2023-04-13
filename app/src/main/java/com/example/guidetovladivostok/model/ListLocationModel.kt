1db15ae6599ee5ec67d9dd57fe6ed50245a5f83d
kirlozavr <kirill.protchenko@mail.ru>
2023-04-13 22:08:49 +1000

    Модель для получения списка локаций

---
+++ app/src/main/java/com/example/guidetovladivostok/model/ListLocationModel.kt
@@ -0,0 +1,35 @@
+package com.example.guidetovladivostok.model
+
+import com.example.guidetovladivostok.dto.MarkerDto
+import com.example.guidetovladivostok.entity.MarkerEntity
+import com.example.guidetovladivostok.exception.ErrorRequestException
+import com.example.guidetovladivostok.mapper.MarkerMapper
+import com.example.guidetovladivostok.presenter.ListLocationContract
+import com.example.guidetovladivostok.presenter.MapContract
+import com.example.guidetovladivostok.repository.GetListLocationRepository
+
+class ListLocationModel
+    : ListLocationContract.Model<CallBackHandler<List<MarkerDto>>>,
+    MapContract.Model<CallBackHandler<List<MarkerDto>>> {
+
+    private val repository = GetListLocationRepository()
+    private val mapper = MarkerMapper()
+
+    override fun getListLocation(callBack: CallBackHandler<List<MarkerDto>>) {
+        repository
+            .getListLocation()
+            .addOnCompleteListener { result ->
+                val listDto = result
+                    .result
+                    .map { value ->
+                        mapper.setDocumentId(value.id)
+                            .getEntityToDto(value.toObject(MarkerEntity::class.java))
+                    }
+                    .toList()
+                callBack.execute(listDto)
+            }
+            .addOnFailureListener {
+                throw ErrorRequestException("Не удалось выполнить запрос")
+            }
+    }
+}
