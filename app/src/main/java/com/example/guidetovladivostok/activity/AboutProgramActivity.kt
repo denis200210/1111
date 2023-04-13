65e2212f9029b2c07c83ecfe1e7ae8e00a4d2369
kirlozavr <kirill.protchenko@mail.ru>
2023-04-13 21:47:07 +1000

    Класс активити с информацией о программе

---
+++ app/src/main/java/com/example/guidetovladivostok/activity/AboutProgramActivity.kt
@@ -0,0 +1,30 @@
+package com.example.guidetovladivostok.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.ViewModelProvider
+import com.example.guidetovladivostok.R
+import com.example.guidetovladivostok.activity.common.NetworkIsConnectedService
+
+class AboutProgramActivity: AppCompatActivity() {
+
+    private lateinit var constraintLayout: ConstraintLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_program)
+        constraintLayout = findViewById(R.id.constraintLayoutAboutActivity)
+        isConnected()
+    }
+
+    private fun isConnected(){
+        val networkIsConnectedService: NetworkIsConnectedService =
+            ViewModelProvider(this)[NetworkIsConnectedService::class.java]
+        networkIsConnectedService.isConnected(
+            networkIsConnectedService,
+            this,
+            constraintLayout
+        )
+    }
+}
