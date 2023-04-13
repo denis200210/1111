ffc659a9651690e199a88e9b47282165db126a02
kirlozavr <kirill.protchenko@mail.ru>
2023-04-13 22:03:15 +1000

    Презентер для работы с активность показа списка локаций

---
+++ app/src/main/java/com/example/guidetovladivostok/presenter/ListLocationPresenter.kt
@@ -0,0 +1,28 @@
+package com.example.guidetovladivostok.presenter
+
+import com.example.guidetovladivostok.dto.MarkerDto
+import com.example.guidetovladivostok.model.CallBackHandler
+import com.example.guidetovladivostok.model.ListLocationModel
+
+class ListLocationPresenter: ListLocationContract.Presenter {
+
+    private lateinit var contractView: ListLocationContract.View<List<MarkerDto>>
+    private val contractModel: ListLocationContract.Model<CallBackHandler<List<MarkerDto>>>
+    private val callBackHandler = object : CallBackHandler<List<MarkerDto>>{
+        override fun execute(value: List<MarkerDto>) {
+            contractView.showListLocation(value)
+        }
+    }
+
+    constructor(
+        contractView: ListLocationContract.View<List<MarkerDto>>
+    ) {
+        this.contractView = contractView
+        this.contractModel = ListLocationModel()
+    }
+
+
+    override fun showListLocation() {
+        contractModel.getListLocation(callBackHandler)
+    }
+}
