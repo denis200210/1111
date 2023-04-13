84a166e92d2b26f4bb92623b17fd7a366c324202
kirlozavr <kirill.protchenko@mail.ru>
2023-04-13 22:05:13 +1000

    Добавил обратный вызов

--- app/src/main/java/com/example/guidetovladivostok/model/ChangeLocationModel.kt
+++ app/src/main/java/com/example/guidetovladivostok/model/ChangeLocationModel.kt
@@ -2,17 +2,18 @@ package com.example.guidetovladivostok.model
 
 import com.example.guidetovladivostok.dto.MarkerDto
 import com.example.guidetovladivostok.exception.ChangeLocationException
+import com.example.guidetovladivostok.presenter.ChangeLocationContract
 import com.example.guidetovladivostok.repository.ChangeLocationRepository
 
-class ChangeLocationModel {
+class ChangeLocationModel: ChangeLocationContract.Model<MarkerDto, CallBackOnClose> {
 
     private val repository = ChangeLocationRepository()
 
-    fun updateMarker(markerDto: MarkerDto){
+    override fun updateMarker(markerDto: MarkerDto, callBackOnClose: CallBackOnClose){
         repository
             .updateMarker(markerDto)
             .addOnSuccessListener {
-
+                callBackOnClose.onClose()
             }.addOnFailureListener{
                 throw ChangeLocationException("Не удалось изменить локацию.")
             }
