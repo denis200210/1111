8bfa52076da9ae24f1694171d821dfff0e5c8350
kirlozavr <kirill.protchenko@mail.ru>
2023-04-13 22:04:14 +1000

    Презентер для работы с измением информации о локации

---
+++ app/src/main/java/com/example/guidetovladivostok/presenter/ChangeLocationPresenter.kt
@@ -0,0 +1,27 @@
+package com.example.guidetovladivostok.presenter
+
+import com.example.guidetovladivostok.dto.MarkerDto
+import com.example.guidetovladivostok.model.CallBackOnClose
+import com.example.guidetovladivostok.model.ChangeLocationModel
+
+class ChangeLocationPresenter
+constructor(contractView: ChangeLocationContract.View)
+    :ChangeLocationContract.Presenter<MarkerDto>{
+
+    private val contractView: ChangeLocationContract.View
+    private val contractModel: ChangeLocationContract.Model<MarkerDto, CallBackOnClose>
+    private val callBackOnClose = object : CallBackOnClose{
+        override fun onClose() {
+            contractView.closeFragment()
+        }
+    }
+
+    init {
+        this.contractView = contractView
+        contractModel = ChangeLocationModel()
+    }
+
+    override fun buttonChangeLocation(value: MarkerDto) {
+        contractModel.updateMarker(value, callBackOnClose)
+    }
+}
