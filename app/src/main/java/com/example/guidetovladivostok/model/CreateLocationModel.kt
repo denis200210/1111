d2ea03e28c9cbc028feda337704196ff4e930336
kirlozavr <kirill.protchenko@mail.ru>
2023-04-13 22:05:25 +1000

    Добавил обратный вызов

--- app/src/main/java/com/example/guidetovladivostok/model/CreateLocationModel.kt
+++ app/src/main/java/com/example/guidetovladivostok/model/CreateLocationModel.kt
@@ -2,17 +2,18 @@ package com.example.guidetovladivostok.model
 
 import com.example.guidetovladivostok.entity.MarkerEntity
 import com.example.guidetovladivostok.exception.CreateLocationException
+import com.example.guidetovladivostok.presenter.CreateLocationContract
 import com.example.guidetovladivostok.repository.CreateLocationRepository
 
-class CreateLocationModel {
+class CreateLocationModel : CreateLocationContract.Model<MarkerEntity, CallBackOnClose>{
 
     private val repository = CreateLocationRepository()
 
-    fun putMarker(markerEntity: MarkerEntity){
+    override fun putMarker(markerEntity: MarkerEntity, callBackOnClose: CallBackOnClose){
         repository
             .putMarker(markerEntity)
             .addOnSuccessListener {
-
+                callBackOnClose.onClose()
             }.addOnFailureListener{
                 throw CreateLocationException("Не удалось добавить локацию.")
             }
